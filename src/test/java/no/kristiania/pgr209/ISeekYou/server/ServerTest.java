8371e208b11ef5109282bd1b01089b3a1ec2c8a2
TobiasLiu1990 <toli025@student.kristiania.no>
2022-11-05 21:33:32 +0100

    Test shouldServeHomePage passes. Removed unnecessary things in App.jsx

--- src/test/java/no/kristiania/pgr209/ISeekYou/server/ServerTest.java
+++ src/test/java/no/kristiania/pgr209/ISeekYou/server/ServerTest.java
@@ -1,11 +1,40 @@
 package no.kristiania.pgr209.ISeekYou.server;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ServerTest {
 
-    @Test
-    void shouldServerHomePage() {
+    private MessageServer server;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        server = new MessageServer(0);
+        server.start();
+    }
+
+    @Test
+    void shouldServerHomePage() throws IOException {
+        var connection = openConnection("/");
+
+        assertThat(connection.getResponseCode())
+                .as(connection.getResponseMessage() + " for " + connection.getURL())
+                .isEqualTo(200);
+
+        assertThat(connection.getInputStream())
+                .asString(StandardCharsets.UTF_8)
+                .contains("<title>I Seek You</title>");
+    }
+
+
+    private HttpURLConnection openConnection(String path) throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), path).openConnection();
     }
 }
